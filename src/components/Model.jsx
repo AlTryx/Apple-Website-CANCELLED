f613a2ef5b948b32056dd35d4421dfac91a12e1c
Aleksandar Stanchev <altryx15@gmail.com>
2024-04-06 23:00:38 +0300

    Update Model.jsx

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -22,6 +22,12 @@ const Model = () => {
     <h1 id ="heading" className = "section-heading">
         Take a closer look.
     </h1>
+<div className = "flex flex-col items-center mt-5">
+    <div className="w-full h-[75v] md:h-[90vh] overflow-hidden relative"></div>
+</div>
+
+
+
 </div>
     </section>
   )
