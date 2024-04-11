6f5c3d0aa924655e738b437ac77cf05505a7bcca
Aleksandar Stanchev <altryx15@gmail.com>
2024-04-11 18:45:33 +0300

    Model and Lights
    The iPhone model is added as well as Lightning. Next step is to make it move and interactable with the user.

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -3,6 +3,9 @@ import React from 'react'
 import gsap from "gsap";
 import { useState } from 'react';
 import { yellowImg } from "../utils";
+import {sizes} from "../constants/index"
+import * as THREE from 'three'
+
 const Model = () => {
 
     const [size, setSize] = useState('small');
@@ -12,6 +15,17 @@ const Model = () => {
     img: yellowImg})
 
         // camera control for the model view
+        // we create refs that will be used in the return ()
+
+        const cameraControlSmall = useRef();
+        const cameraControlLarge = useRef();
+
+        // the models themselves
+        const small = useRef(new THREE.Group())
+        const large = useRef(new THREE.Group())
+        // rotation
+        const [smallRotation, setSmallRotation] = useState(0);
+        const [largeRotation, setLargeRotation] = useState(0);
 
     useGSAP(() => {
         gsap.to('#heading', { 
@@ -28,7 +42,64 @@ const Model = () => {
     </h1>
 <div className = "flex flex-col items-center mt-5">
     <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
-        <ModelView />
+        <ModelView
+        index = {1}
+        groupRef={small}
+        gsapType="view1"
+        controlRef={cameraControlSmall}
+        setRotationState={setSmallRotation}
+        item={model}
+        size = {size}
+        />
+
+        <ModelView
+        index = {2}
+        groupRef={large}
+        gsapType="view2"
+        controlRef={cameraControlLarge}
+        setRotationState={setLargeRotation}
+        item={model}
+        size = {size}
+        />
+        <Canvas
+            className = "w-full h-full" style = {
+                {
+                    position: 'fixed',
+                    top: 0,
+                    bottom:0,
+                    left:0,
+                    right:0,
+                    overflow:'hidden'
+                }}
+                eventSource = {document.getElementById('root')}
+        >
+            <View.Port />
+        </Canvas>
+    </div>
+    <div className= "mx-auto w-full">
+                <p className= "text-sm font-light text-center mb-5">{model.title}</p>
+                <div className = "flex-center">
+                    <ul className = "color-container">
+                        {models.map((item, i)=> 
+                    <li key = {i} className = "w-6 h-6 rounded-full mx-2 cursor-pointer" style = 
+                    {{
+                        backgroundColor: item.color[0]
+                    }}
+                        onClick = {() => setModel(item)}/>
+
+
+                        )}
+                    </ul>
+                    <button className = "size-btn-container">
+                        {sizes.map(({label,value})=> (
+                            <span key = {label} className = "size-btn" style = {{
+                                backgroundColor: size === value ? 'white ' : 'transparent', color : size === value ? 'black' : 'white' 
+                            }} onClick = {()=> setSize (value)} >
+                                {label}
+                            </span>
+                        ))}
+                    </button>
+                </div>
     </div>
 </div>
 </div>
