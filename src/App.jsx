be6f6c71cf1c3c7ae421f188f494b6af7c899983
Aleksandar Stanchev <altryx15@gmail.com>
2024-04-14 14:51:17 +0300

    Sentry is being installed

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Model from './components/Model';
 const App = () => {
+
+  
   return (
     <main className = "bg-black">
     < Navbar />
