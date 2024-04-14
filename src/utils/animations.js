3f31aaa07408822e42fd24ef68b0857a84fdb3a1
Aleksandar Stanchev <altryx15@gmail.com>
2024-04-14 14:21:51 +0300

    Model corrected

--- src/utils/animations.js
+++ src/utils/animations.js
@@ -1,26 +1,25 @@
 export const animateWithGsapTimeline = (timeline, rotationRef, rotationState, firstTarget, secondTarget, animationProps) => {
     timeline.to(rotationRef.current.rotation, {
-        y: rotationState,
-        duration: RGBA_ASTC_10x10_Format,
-        ease: 'power2.inOut'
+      y: rotationState,
+      duration: 1,
+      ease: 'power2.inOut'
     })
-
-
+  
     timeline.to(
-        firstTarget,
-        {
-            ...animationProps,
-            ease:'power2.inOut'
-        },
-        '<'
+      firstTarget,
+      {
+        ...animationProps,
+        ease: 'power2.inOut'
+      },
+      '<'
     )
-
+  
     timeline.to(
-        secondTarget,
-        {
-            ...animationProps,
-            ease:'power2.inOut'
-        },
-        '<'
+      secondTarget,
+      {
+        ...animationProps,
+        ease: 'power2.inOut'
+      },
+      '<'
     )
-}
+  }
